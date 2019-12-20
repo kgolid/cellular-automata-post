a4a4063f722c46397d805f3deadfe452f4f12dd9
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2019-12-20 03:06:28 +0100

    rewrite some of the example code

--- automata.js
+++ automata.js
@@ -2,7 +2,7 @@
 const get_bit = (num, pos) => (num >> pos) & 1;
 
 // Combines 3 bits into an integer between 0 and 7
-const combine = (b1, b2, b3) => b1 * 4 + b2 * 2 + b3;
+const combine = (b1, b2, b3) => (b1 << 2) + (b2 << 1) + (b3 << 0);
 
 // Returns given number in the form of a tertiary function (a rule)
 export const get_rule = num => (b1, b2, b3) => get_bit(num, combine(b1, b2, b3));
--- index.js
+++ index.js
@@ -1,53 +1,73 @@
 import { get_rule } from './automata';
 
-const canvas_width = 1000;
-const canvas_height = 500;
-const grid_dim = 100; // Number of cells in the dimension of the grid.
-
-const cell_scale = canvas_width / grid_dim;
-const iterations = canvas_height / cell_scale;
-
 window.onload = function() {
-  const canvas = setup_canvas(document);
+  const width = 1000;
+  const height = 500;
+
+  const cells_across = 200;
+  const cell_scale = width / cells_across;
+  const cells_down = height / cell_scale;
+
+  const rule = get_rule(30);
+
+  const canvas = setup_canvas(document, width, height);
   document.body.appendChild(canvas);
 
-  draw_rule(canvas.getContext('2d'), get_rule(30));
+  const context = canvas.getContext('2d');
+  draw_rule(context, rule, cell_scale, cells_across, cells_down);
 };
 
-function draw_rule(ctx, rule) {
-  let row = initial_row();
-  for (let i = 0; i < iterations; i++) {
-    draw_row(ctx, row);
+function draw_rule(ctx, rule, scale, width, height) {
+  let row = initial_row(width);
+  for (let i = 0; i < height; i++) {
+    draw_row(ctx, row, scale);
+    s;
     row = next_row(row, rule);
   }
 }
+/*
+function draw_rule(ctx, rule) {
+  make_grid(rule).forEach(r => draw_row(ctx, r));
+}
 
-function draw_row(ctx, row) {
+function make_grid(rule, height) {
+  return [...Array(height)].reduce(
+    (acc, _) => [...acc, next_row(acc[acc.length - 1], rule)],
+    [initial_row()]
+  );
+}
+*/
+
+function draw_row(ctx, row, scale) {
   ctx.save();
   row.forEach(cell => {
-    ctx.fillStyle = cell == 1 ? '#000' : '#fff';
-    ctx.fillRect(0, 0, cell_scale, cell_scale);
-    ctx.translate(cell_scale, 0);
+    ctx.fillStyle = cell === 1 ? '#000' : '#fff';
+    ctx.fillRect(0, 0, scale, scale);
+    ctx.translate(scale, 0);
   });
   ctx.restore();
-  ctx.translate(0, cell_scale);
+  ctx.translate(0, scale);
 }
 
 function next_row(old, rule) {
   return old.map((_, i) => rule(old[i - 1], old[i], old[i + 1]));
 }
 
-function initial_row() {
-  const initial_row = Array(grid_dim).fill(0);
-  initial_row[Math.floor(grid_dim / 2)] = 1;
+function initial_row(width) {
+  const initial_row = Array(width).fill(0);
+  initial_row[Math.floor(width / 2)] = 1;
 
   return initial_row;
 }
 
-function setup_canvas(document) {
+function initial_random_row(width) {
+  return Array.from(Array(width), _ => Math.floor(Math.random() * 2));
+}
+
+function setup_canvas(document, width, height) {
   var canvas = document.createElement('canvas');
-  canvas.width = canvas_width;
-  canvas.height = canvas_height;
+  canvas.width = width;
+  canvas.height = height;
 
   return canvas;
 }
